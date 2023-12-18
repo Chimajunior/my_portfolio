3f3e1f6f485a131267982a11bdef00fd3147f3df
Chims <chimajunior14@gmail.com>
2023-12-19 00:37:48 +0100

    language helper for the component

---
+++ src/helpers/LangList.jsx
@@ -0,0 +1,59 @@
+import { Icon } from '@iconify/react';
+
+export const FrontList = [
+  {
+    icon: <Icon icon="skill-icons:html" />,
+    content: "HTML",
+  },
+  {
+    icon: <Icon icon="skill-icons:css" />,
+    content: "CSS",
+  },
+  {
+    icon: <Icon icon="skill-icons:javascript"  />,
+    content: "JAVASCRIPT",
+  },
+  
+  {
+    icon: <Icon icon="skill-icons:python-dark"  />,
+    content: "PYTHON",
+  },
+];
+
+export const FrameList = [
+  {
+    icon: <Icon icon="skill-icons:tailwindcss-dark" />,
+    content: "TAILWIND",
+  },
+  {
+    icon: <Icon icon="skill-icons:react-dark" />,
+    content: "REACT",
+  },
+]
+
+
+export const TechList = [
+  {
+    icon: <Icon icon="logos:figma" />,
+    content: "Figma",
+  },
+  {
+    icon: <Icon icon="devicon:git" />,
+    content: "Git",
+  },
+  {
+    icon: <Icon icon="icon-park:github" />,
+    content: "GitHub",
+  },
+  
+ {
+    icon: <Icon icon="skill-icons:vscode-dark" />,
+    content: "VSCode",
+  },
+  
+  {
+    icon: <Icon icon="logos:pycharm" />,
+    content: "Pycharm",
+  },
+ 
+];
