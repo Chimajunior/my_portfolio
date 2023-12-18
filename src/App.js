25865595f065c9b52acdb6df6465888f305fc19e
Chims <chimajunior14@gmail.com>
2023-12-18 14:56:44 +0100

    removed logo, added ts-particles and also added aos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+import React, {useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 function App() {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    await loadFull(main);
+  };
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
+
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          particles: {
+            number: {
+              value: 400,
+              density: {
+                enable: true,
+                value_area: 1000,
+              },
+            },
+            color: {
+              value: "#ccf",
+              // value: "#fff",
+            },
+            opacity: {
+              value: 0.04,
+              anim: {
+                enable: true,
+              },
+            },
+            size: {
+              value: 7,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 10,
+              },
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              speed: 1,
+            },
+          },
+        }}
+      />
     </div>
   );
 }
