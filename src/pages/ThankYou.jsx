2f614143a5a6d06fa0759b3025be8117b846ae9a
Chims <chimajunior14@gmail.com>
2023-12-19 00:15:56 +0100

    added a thank you page

---
+++ src/pages/ThankYou.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/Thanks.css";
+
+
+function ThankYou() {
+  return (
+    <div className='tY'>
+        <div className='heroo'>
+      <h1>Thank you for contacting me!</h1>
+      <h2>I'll be in touch with you shortly</h2>
+    </div></div>
+  )
+}
+
+export default ThankYou
