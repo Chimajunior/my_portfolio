9066b7c9a61168f488ddf0e6d0426615b29a47a8
Chims <chimajunior14@gmail.com>
2023-12-18 14:59:29 +0100

    added a footer to the end of the page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../styles/Footer.css"
+function Footer() {
+    return (
+      <div className="footer">
+        <p>&copy; chima.jr 2023 </p>
+      </div>
+    );
+  }
+  
+  export default Footer;
