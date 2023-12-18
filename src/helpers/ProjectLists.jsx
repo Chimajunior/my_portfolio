ca7e8192557e20c69e003afa3f9fdea5fe34b1f9
Chims <chimajunior14@gmail.com>
2023-12-19 00:37:17 +0100

    projectList helpers for the component

---
+++ src/helpers/ProjectLists.jsx
@@ -0,0 +1,43 @@
+import Pizza from "../assets/pizzaLogo.png";
+import CLS from "../assets/cls_5g_home.png";
+import Burger from "../assets/chima_Jr_diner.png";
+import Portfolio from "../assets/portfolio.png";
+
+export const ProjectList = [
+  {
+    name: "ClS 5G Telecom",
+    image: CLS,
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus iste ipsa molestiae cupiditate consectetur itaque, veniam deleniti unde perspiciatis aspernatur id? Explicabo, sequi nostrum fuga dolores magnam tempora doloribus hic.",
+    site: <a href="https://d7bed9ee.cls-5g-telecommunication.pages.dev/" target="_blank"  rel="noreferrer">Visit site</a>,
+  },
+  {
+    name: "Chima jr's Diner",
+    image: Burger,
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus iste ipsa molestiae cupiditate consectetur itaque, veniam deleniti unde perspiciatis aspernatur id? Explicabo, sequi nostrum fuga dolores magnam tempora doloribus hic.",
+    site: <a href="" target="_blank">Visit site</a>,
+
+  },
+  {
+    name: "My Portfolio",
+    image: Portfolio,
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus iste ipsa molestiae cupiditate consectetur itaque, veniam deleniti unde perspiciatis aspernatur id? Explicabo, sequi nostrum fuga dolores magnam tempora doloribus hic.",
+    site: <a href="#">Visit site</a>,
+  },
+  {
+    name: "ClS 5G",
+    image: Pizza,
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus iste ipsa molestiae cupiditate consectetur itaque, veniam deleniti unde perspiciatis aspernatur id? Explicabo, sequi nostrum fuga dolores magnam tempora doloribus hic.",
+    site: <a href="" target="_blank">Visit site</a>,
+  },
+  {
+    name: "Landing page",
+    image: Pizza,
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus iste ipsa molestiae cupiditate consectetur itaque, veniam deleniti unde perspiciatis aspernatur id? Explicabo, sequi nostrum fuga dolores magnam tempora doloribus hic.",
+    site: <a href="" target="_blank">Visit site</a>,
+  },
+];
