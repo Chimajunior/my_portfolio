c31d0f35d0be494caae51a313733089a6e08b931
Chims <chimajunior14@gmail.com>
2024-02-03 00:11:22 +0100

    comm

--- src/helpers/ProjectLists.jsx
+++ src/helpers/ProjectLists.jsx
@@ -2,8 +2,25 @@ import Pizza from "../assets/pizzaLogo.png";
 import CLS from "../assets/cls_5g_home.png";
 import Burger from "../assets/chima_Jr_diner.png";
 import Portfolio from "../assets/portfolio.png";
+import Watch from "../assets/watch_site.png";
+import Dark from "../assets/Dark.png";
+
 
 export const ProjectList = [
+  {
+    name: "Responsive Watch site",
+    image: Watch,
+    content:
+      "Explore my website showcasing my impactful contributions to the 5G Telecom Project, demonstrating innovative solutions and cutting-edge technologies in the telecommunications industry.",
+    site: <a href="https://responsive-watch-web.vercel.app/?#home" target="_blank"  rel="noreferrer">Visit site</a>,
+  },
+  {
+    name: "Darkagys",
+    image: Dark,
+    content:
+      "Explore my website showcasing my impactful contributions to the 5G Telecom Project, demonstrating innovative solutions and cutting-edge technologies in the telecommunications industry.",
+    site: <a href="https://responsive-watch-web.vercel.app/?#home" target="_blank"  rel="noreferrer">Visit site</a>,
+  },
   {
     name: "ClS 5G Telecom",
     image: CLS,
@@ -41,3 +58,4 @@ export const ProjectList = [
     site: <a href="" target="_blank">Visit site</a>,
   },
 ];
+
